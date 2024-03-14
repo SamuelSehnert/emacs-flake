f50f2fab78617f66548c0b01475341ba277d5e02
coffeestainedcode <mail@coffeestainedcode.com>
2024-03-13 19:52:37 -0700

    Remove download directory after racketPkg update

--- lib/racketWithPkgs/update.sh
+++ lib/racketWithPkgs/update.sh
@@ -3,17 +3,18 @@
 
 # How to run: nix-shell
 
-echo "Creating temporary directory"
+echo "Creating download directory"
 mkdir -p ./downloads
 
 echo "Gathering materials"
 curl "https://download.racket-lang.org/releases/8.12/catalog/pkgs-all" --output ./downloads/release-catalog.lock
 curl "https://pkgs.racket-lang.org/pkgs-all?version=8.12" --output ./downloads/live-catalog.lock
 
-echo "Parsing outputs"
+echo "Building results"
 racket ./update.rkt
 
 echo "Formatting result"
 nixpkgs-fmt ./racket-package-defs.nix
 
-echo "Done! Thanks for updating!"
+echo "Removing directory"
+rm -rf ./downloads
